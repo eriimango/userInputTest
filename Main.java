be28ed7a62d5d439c6fd1204d71a32a2dc72a40c
Ericka <64378078+eriimango@users.noreply.github.com>
2021-10-11 02:58:15 +0000

    update code

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-
+// Simple user input java test
+// author Ericka B.H
 import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     Scanner myExp = new Scanner(System.in);
     String expression;
-    
-    // Enter username and press Enter
+    System.out.println("This is a simple user input program");
+    System.out.println("");
+    // user input
     System.out.println("Enter an arithemtic expression using only binary arithmitic operators: "); 
     expression = myExp.nextLine();   
-       
-    System.out.println("Expression evaluated: " + expression);        
+    // user result   
+    System.out.println("Your Expression: " + expression);        
   }
 }
